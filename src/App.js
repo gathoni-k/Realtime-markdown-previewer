76c5c17090375ecf35cdf193ecc977932d2cacde
gathoni-k <maryg.kahiga@gmail.com>
2020-07-04 16:38:09 +0300

    Convert markdown to html

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import './App.css';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import showdown from 'showdown'
+
+const converter  = new showdown.Converter()
 
 function App() {
+  React.useEffect(() => {
+    const editPad = document.getElementById('edit-pad')
+    const markdownPreview = document.getElementById('markdown-preview')
+
+    const convertToMarkdown = () => {
+      let mdText = editPad.value
+      let html = converter.makeHtml(mdText)
+      markdownPreview.innerHTML = html
+    }
+    editPad.addEventListener('input', convertToMarkdown);
+    convertToMarkdown()
+  })
   return (
     <Container fluid>
       <Row>
         <Col className='pad'>
-          <textarea className='full-height mt-3 edit-pad' placeholder='Edit...'></textarea>
+          <textarea className='full-height mt-3' id='edit-pad' placeholder='Edit...'></textarea>
         </Col>
         <Col className='markdown'>
           <header className='header'>Preview Markdown</header>
