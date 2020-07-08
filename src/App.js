3ffca4d914b6c23810781b4e6d177bc6749fa5f0
gathoni-k <maryg.kahiga@gmail.com>
2020-07-08 23:52:42 +0300

    Refactor components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,18 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import showdown from 'showdown'
-
-const converter  = new showdown.Converter()
+import './styles/App.css';
+import Navbar from './Navbar'
+import Pad from './Pad'
 
 function App() {
   React.useEffect(() => {
-    const editPad = document.getElementById('edit-pad')
-    const markdownPreview = document.getElementById('markdown-preview')
 
-    const convertToMarkdown = () => {
-      let mdText = editPad.value
-      let html = converter.makeHtml(mdText)
-      markdownPreview.innerHTML = html
-    }
-    editPad.addEventListener('input', convertToMarkdown);
-    convertToMarkdown()
   })
   return (
-    <Container fluid>
-      <Row>
-        <Col className='pad' xs={12} md={6}>
-        <header className='header'>Markdown</header>
-          <textarea className='full-height mt-3' id='edit-pad' placeholder='Edit...'></textarea>
-        </Col>
-        <Col className='markdown'>
-          <header className='header'>Preview Markdown</header>
-          <div id='markdown-preview'></div>
-        </Col>
-      </Row>
-    </Container>
+    <div className='px-8 sm:px-2 md:px-4 lg:px-12 xl:px-16'>
+      <Navbar/>
+      <Pad/>
+    </div>
   );
 }
 
