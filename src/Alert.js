cd45a1874ff5afdd3b797e02175e84aec171f792
gathoni-k <maryg.kahiga@gmail.com>
2020-07-08 23:54:15 +0300

    Create aert component

---
+++ src/Alert.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Alert(props) {
+    if (!props.warn) {
+        return null;
+      }
+    return (
+        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+            <span className="block sm:inline">Provide post title.</span>
+        </div>
+    )
+};
