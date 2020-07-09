2a3f4674cdb4bbac29e686cbadcad989d58ebfa8
gathoni-k <maryg.kahiga@gmail.com>
2020-07-09 22:02:27 +0300

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
 import './styles/App.css';
 import Navbar from './Navbar'
 import Pad from './Pad'
+import Posts from './Posts'
+import NotFound from './NotFound';
+import Edit from './Edit'
 
 function App() {
-  React.useEffect(() => {
-
-  })
   return (
     <div className='px-8 sm:px-2 md:px-4 lg:px-12 xl:px-16'>
       <Navbar/>
-      <Pad/>
+      <Switch>
+      <Route exact path='/'><Posts/></Route>
+      <Route path='/write'><Pad/></Route>
+      <Route path='/edit/:id' ><Edit/></Route>
+      <Route><NotFound/></Route>
+      </Switch>
     </div>
   );
 }
