04770278f0a0eaff6c137b76ba221f4e706eb959
gathoni-k <maryg.kahiga@gmail.com>
2020-07-09 22:04:26 +0300

    Change color scheme

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav className='px-1 pt-3 pb-1 flex flex-row sm:flex-col md:flex-row lg:flex-row xl:flex-row justify-between border-b-2 border-gray-800 border-text-flame'>
+    <nav className='px-1 pt-3 pb-1 flex flex-row sm:flex-col md:flex-row lg:flex-row xl:flex-row justify-between border-b-2 bg-flame-border-b'>
       <div className="flex">
-        <i className='fa fa-code-branch mr-2 text-2xl text-flame'></i>
+        <i className='fa fa-code-branch mr-2 text-2xl text-green-500'></i>
         <a className=" text-black hover:text-black text-xl hover:no-underline" href="/">tuts</a>
       </div>
       <ul className="flex mt-1 justify-between">
       <li className="mr-6">
-        <a className="text-flame font-bold hover:no-underline hover:text-black" href="/">Write</a>
+        <Link to='/write' className="text-green-500 hover:text-green-600 font-bold hover:no-underline hover:text-black" >Write</Link>
       </li>
       <li className="mr-6">
-        <a className="text-flame font-bold hover:no-underline hover:text-black" href="/">Posts</a>
+        <Link to='/' className="text-green-500 hover:text-green-600 font-bold hover:no-underline hover:text-black">Posts</Link>
       </li>
     </ul>
     </nav>
