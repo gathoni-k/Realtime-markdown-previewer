cfda842189b0768c9307c8686618eb9c19a79a7c
gathoni-k <maryg.kahiga@gmail.com>
2020-07-09 22:04:02 +0300

    Create a loader component

---
+++ src/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Loader() {
+
+    return (
+        <div className='flex items-center justify-center mt-16'>
+            <img src='./frog.svg' alt='' className='h-24 w-24 loader'/>
+        </div>
+    )
+}
